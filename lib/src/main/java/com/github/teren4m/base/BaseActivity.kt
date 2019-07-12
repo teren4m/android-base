0600092b85090468d50b6d81472e20688af48aa5
Tim Andriyaschenko <timAndriyaschenko@gmail.com>
2019-07-12 06:22:38 +0300

    Fix

--- lib/src/main/java/com/github/teren4m/base/BaseActivity.kt
+++ lib/src/main/java/com/github/teren4m/base/BaseActivity.kt
@@ -3,7 +3,6 @@ package com.github.teren4m.base
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import org.koin.android.ext.android.get
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -16,8 +15,6 @@ abstract class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(layoutId)
         setSupportActionBar(toolbar)
-
-        val some: String = get()
     }
 
 }
